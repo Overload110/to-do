7d2c79038b66cf25fc0972e2fe7828871309451a
Overload110 <149197288+Overload110@users.noreply.github.com>
2024-01-04 12:48:54 -0600

    Create list cards

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import { add } from 'date-fns';
+import { add, format } from 'date-fns';
 import {soonList, futureList} from "./todo-list";
+import { Todo } from './todo-item';
 
 const shortTerm = document.getElementById('short-term');
 const longTerm = document.getElementById('long-term');
@@ -31,6 +32,46 @@ document.getElementById("todoForm").addEventListener("submit", function(e) {
 
   let newTodo = new Todo(title, desc, due, priority);
   addItem(newTodo);
+  refreshLists();
 
   dialog.close(); // Close the dialog after adding a task
 });
+
+function refreshLists(){
+  shortTerm.innerHTML = '';
+  longTerm.innerHTML = '';
+  soonList.forEach((element) => {
+    shortTerm.appendChild(createTodoDiv(element));
+  });
+  futureList.forEach((element) => {
+    longTerm.appendChild(createTodoDiv(element));
+  });
+}
+
+function createTodoDiv(todo){
+  const itemDiv = document.createElement('div');
+    itemDiv.classList.add('todo', todo.priority);
+
+  const task = document.createElement('h4');
+  task.classList.add('task');
+  task.innerHTML = todo.title;
+
+  const description = document.createElement('p');
+  description.classList.add('description');
+  description.innerHTML = todo.desc;
+
+  const dateDue = document.createElement('p');
+  dateDue.innerHTML = format(todo.dueDate);
+  dateDue.classList.add('dueDate');
+
+  const priority = document.createElement('p');
+  priority.innerHTML = todo.priority;
+  priority.classList.add('priority');
+
+  itemDiv.appendChild(task);
+  itemDiv.appendChild(description);
+  itemDiv.appendChild(dateDue);
+  itemDiv.appendChild(priority);
+
+  return itemDiv;
+}
