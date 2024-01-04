783d8b06357bd8afe990277d1c29c921ba0c57f3
Overload110 <149197288+Overload110@users.noreply.github.com>
2024-01-04 16:30:26 -0600

    Add remove button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { add, format } from 'date-fns';
-import {soonList, futureList} from "./todo-list";
+import {format } from 'date-fns';
+import {soonList, futureList, addItem, removeItem} from "./todo-list";
 import { Todo } from './todo-item';
 
 const shortTerm = document.getElementById('short-term');
@@ -16,28 +16,22 @@ addButton.addEventListener('click', () => {
     dialog.close();
   });
 
-
-function showLists(soon, future){
-    soon.array.forEach(element => {
-        
-    });
-}
-
 document.getElementById("todoForm").addEventListener("submit", function(e) {
   e.preventDefault();
   const title = document.getElementById("title").value;
   const desc = document.getElementById("desc").value;
   const due = document.getElementById("dueDate").value;
+  console.log(due);
   const priority = document.getElementById("priority").value;
 
   let newTodo = new Todo(title, desc, due, priority);
   addItem(newTodo);
-  refreshLists();
+  showLists();
 
   dialog.close(); // Close the dialog after adding a task
 });
 
-function refreshLists(){
+function showLists(){
   shortTerm.innerHTML = '';
   longTerm.innerHTML = '';
   soonList.forEach((element) => {
@@ -51,6 +45,7 @@ function refreshLists(){
 function createTodoDiv(todo){
   const itemDiv = document.createElement('div');
     itemDiv.classList.add('todo', todo.priority);
+    itemDiv.id = todo.id;
 
   const task = document.createElement('h4');
   task.classList.add('task');
@@ -61,17 +56,25 @@ function createTodoDiv(todo){
   description.innerHTML = todo.desc;
 
   const dateDue = document.createElement('p');
-  dateDue.innerHTML = format(todo.dueDate);
+  dateDue.innerHTML = format(todo.dueDate, 'MMM dd, yyyy');
   dateDue.classList.add('dueDate');
 
   const priority = document.createElement('p');
   priority.innerHTML = todo.priority;
   priority.classList.add('priority');
 
+  const remove = document.createElement('button');
+  remove.innerHTML = "Remove";
+  remove.addEventListener('click', function() {
+    removeItem(todo);
+    itemDiv.remove();
+  });
+
   itemDiv.appendChild(task);
   itemDiv.appendChild(description);
   itemDiv.appendChild(dateDue);
   itemDiv.appendChild(priority);
+  itemDiv.appendChild(remove);
 
   return itemDiv;
 }
