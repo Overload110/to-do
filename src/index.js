831e5b305b657e63590677806bc0ff6244e64dbd
Overload110 <149197288+Overload110@users.noreply.github.com>
2024-01-05 12:44:52 -0600

    Add done checkbox

--- src/index.js
+++ src/index.js
@@ -49,31 +49,47 @@ function createTodoDiv(todo){
 
   const task = document.createElement('h4');
   task.classList.add('task');
-  task.innerHTML = todo.title;
+  task.innerHTML = `Task: ${todo.title}`;
 
   const description = document.createElement('p');
   description.classList.add('description');
   description.innerHTML = todo.desc;
 
   const dateDue = document.createElement('p');
-  dateDue.innerHTML = format(todo.dueDate, 'MMM dd, yyyy');
+  dateDue.innerHTML = `Due: ${format(todo.dueDate, 'MMM dd, yyyy')}`;
   dateDue.classList.add('dueDate');
 
   const priority = document.createElement('p');
-  priority.innerHTML = todo.priority;
+  priority.innerHTML = `Priority: ${todo.priority}`;
   priority.classList.add('priority');
 
   const remove = document.createElement('button');
-  remove.innerHTML = "Remove";
+  remove.innerHTML = "X";
   remove.addEventListener('click', function() {
     removeItem(todo);
     itemDiv.remove();
   });
 
+  const doneBox = document.createElement('input');
+  doneBox.type = "checkbox";
+  doneBox.id = "doneBox";
+  doneBox.checked = todo.done;
+  const doneLabel = document.createElement('label');
+  doneLabel.setAttribute('for', doneBox.id);
+  doneLabel.innerHTML = 'Done: '
+  doneBox.addEventListener('change', function() {
+    todo.done = this.checked;
+    localStorage.setItem("futureList", JSON.stringify(futureList));
+    localStorage.setItem("soonList", JSON.stringify(soonList));
+  });
+
+
   itemDiv.appendChild(task);
   itemDiv.appendChild(description);
   itemDiv.appendChild(dateDue);
   itemDiv.appendChild(priority);
+  itemDiv.appendChild(doneLabel);
+  itemDiv.appendChild(doneBox);
   itemDiv.appendChild(remove);
 
   return itemDiv;
